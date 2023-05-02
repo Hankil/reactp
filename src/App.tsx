077c1c4f26a81749b42e2d018ec699835a1c58e1
idisgh <idisgh@naver.com>
2023-05-02 22:34:23 +0900

    emotion 패키지 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled from "@emotion/styled";
+
+const H1 = styled.h1`
+    span {
+      color: coral;
+      + span {
+        color: darkviolet;
+      }
+    }
+  
+`
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +27,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React2</h1>
+      <H1><span>Vite</span> + <span>React2</span></H1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
